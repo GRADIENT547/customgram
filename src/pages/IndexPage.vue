a05283597f93f5b5a57462527ff7eee2b8258828
gradient <elizararsen@gmail.com>
2025-04-01 12:07:07 +0400

    add base messenger templates for messages and input

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,59 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center column">
+    <div class="q-pa-md row justify-center" style="width: 100%">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          v-for="message in messages"
+          :key="message.id"
+          :name="message.name"
+          :text="[message.text]"
+          :sent="message.sent"
+        />
+      </div>
+    </div>
+
+    <div class="q-pa-md row justify-center" style="width: 100%">
+      <div style="width: 100%; max-width: 400px">
+        <q-input
+          bottom-slots
+          v-model="text"
+          label="Enter your message"
+          outlined
+          class="q-mt-md"
+        >
+          <template v-slot:after>
+            <q-btn round dense flat icon="send" />
+          </template>
+        </q-input>
+      </div>
+    </div>
   </q-page>
-  <h2>Hello world!</h2><h2>Hello world!</h2><h2>Hello world!</h2>
 </template>
 
+
 <script setup>
-//
+import { ref } from 'vue';
+
+const text = ref('');
+
+const messages = ref([
+  {
+    "id": "1",
+    "name": "me",
+    "text": "How are you?",
+    "sent": true,
+  },
+  {
+    "id": "2",
+    "name": "Jane",
+    "text": "Fine, how are you?",
+    "sent": false,
+  },
+  {
+    "id": "3",
+    "name": "me",
+    "text": "Good, to hear it!",
+    "sent": true,
+  },
+]);
 </script>
